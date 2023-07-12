235d255759d8c66ad0d1d1ed462bfe3d909af03b
Jordan Lopez <jordanlopezemail@gmail.com>
2023-07-12 02:04:59 -0700

    finished with main section

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var searchBtnEl = $("#searchBtn");
 var lat;
 var long;
 
+function displayHistory(){
+
+}
+
+
 function getLat(name) {
     return new Promise((resolve, reject) => {
       var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit=5&appid=9bce6875713db412816a04531af13ead";
@@ -14,7 +19,6 @@ function getLat(name) {
         })
         .then(function(data) {
           var lat = data[0].lat;
-          console.log(lat);
           resolve(lat);
         })
         .catch(function(error) {
@@ -32,7 +36,6 @@ function getLat(name) {
         })
         .then(function(data) {
           var lon = data[0].lon;
-          console.log(lon);
           resolve(lon);
         })
         .catch(function(error) {
@@ -41,7 +44,7 @@ function getLat(name) {
     });
   }
 //MAIN FUNCTIONS ---------------------------------------------------------------------------------
-function renderHistory(name){
+function renderSearch(name, latitude, longitude){
     if (name === ""){
         return;
     }
@@ -51,33 +54,56 @@ function renderHistory(name){
         newEl.attr("class", "historyBtn");
         $("#history").append(newEl);
     }
-}
-function renderMain(name){
-    $("#main h2").text(name);
-}
-function renderFiveDay(name){
+    localStorage.setItem(name, JSON.stringify({lat: latitude, lon: longitude}));
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+function renderMain(name, latitude, longitude){
+    $("#main h2").text(name);
+
+    var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=9bce6875713db412816a04531af13ead&units=imperial";
+    
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+            $("#main h2").text(name +" " + dayjs().format('DD/MM/YY'));
+            $("#main h4").first().text("Temp: "+ data.main.temp + " °f");
+            $("#main h4").eq(1).text("Wind: "+ data.wind.speed + " mph");
+            $("#main h4").eq(2).text("Humidity: "+ data.main.humidity + " %");
+        });
+
+    
+// use the new resolve reject case
+// to generate the 
+
+}
+
+
+
+function renderFiveDay(name, latitude, longitude){
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
 searchBtnEl.on("click", async function() {
     var inputCity = $("#inputCity").val();
-
     var lat = await getLat(inputCity);
     var lon = await getLon(inputCity);
-    console.log(lat + " " + lon);
-
-    renderHistory(inputCity);
-    renderMain(inputCity);
-    renderFiveDay(inputCity);
+    console.log(lat);
+    console.log(lon);
+    renderSearch(inputCity, lat, lon);
+    renderMain(inputCity, lat, lon);
+    renderFiveDay(inputCity, lat, lon);
 
 
     
   });
 
-// on click "search"
-    //render history
-        // create a button with that city name
-        // save it to local storage
-    // render main
-    // render 5-day forecast
+
+function init(){
+    displayHistory(); //displays history from local storage and appends to display div
+} 
+init();
 
 });
